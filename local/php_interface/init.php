c04a55ffc62f8d306edba94a2cdc7bc314e90460
ADTden <33018048+ADTden@users.noreply.github.com>
2020-11-05 10:47:34 +0300

    Записывать в Журнал событий открытие не существующих страниц сайта
    Задача выполнена

--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -20,4 +20,25 @@ class MyClass
 		}
     }
 }
+
+AddEventHandler('main', 'OnEpilog', '_Check404Error', 1);
+function _Check404Error(){
+ if (defined('ERROR_404') && ERROR_404 == 'Y') {
+	
+ global $APPLICATION;
+  CEventLog::Add(array(
+         "SEVERITY" => "INFO",
+         "AUDIT_TYPE_ID" => "ERROR_404",
+         "MODULE_ID" => "main",
+         "DESCRIPTION" => $APPLICATION->GetCurPage(),
+      ));
+ $APPLICATION->RestartBuffer();
+ include $_SERVER['DOCUMENT_ROOT'] . SITE_TEMPLATE_PATH . '/header.php';
+ include $_SERVER['DOCUMENT_ROOT'] . '/404.php';
+ include $_SERVER['DOCUMENT_ROOT'] . SITE_TEMPLATE_PATH . '/footer.php';
+
+ }
+}
+
+
 ?>
