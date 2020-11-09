c8e4de4f1cc48ff8a1f3e8f5b010862f4287963c
ADTden <33018048+ADTden@users.noreply.github.com>
2020-11-09 17:52:08 +0300

    Создание комплексного компонента «Моя фотогалерея»
    Доработать

--- ex2/time_control/index.php
+++ ex2/time_control/index.php
@@ -1,12 +1,14 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Оценка производительности");
-?>Больше всего времени потребовалось странице&nbsp;<a href="http://testtusk/bitrix/admin/perfmon_hit_list.php?lang=ru&set_filter=Y&find_script_name=%2Fex2%2Fsimplecomp%2Findex.php">/ex2/simplecomp/index.php</a>&nbsp;- 3.27 сек.<br>
+?>Больше всего времени потребовалось странице&nbsp;<a href="http://testtusk/bitrix/admin/perfmon_hit_list.php?lang=ru&set_filter=Y&find_script_name=%2Fex2%2Fsimplecomp%2Findex.php">/ex2/simplecomp/index.php</a>&nbsp;- 2.88 сек.<br>
  <br>
  Самая ресурсоемкая страница -&nbsp;&nbsp;<a href="http://testtusk/bitrix/admin/perfmon_hit_list.php?lang=ru&set_filter=Y&find_script_name=%2Fex2%2Fsimplecomp%2Findex.php">/ex2/simplecomp/index.php</a><br>
  Нагрузка&nbsp;14.24%<br>
  <br>
+ Всего SQL запросов:&nbsp;133<br>
+ <br>
  Самый ресурсоемкий компонент<br>
- Компонент&nbsp;dv:simplecomp.exam:<br>
- <nobr>0.136 с</nobr>;&nbsp;<br>
-<br><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+ dv:simplecomp.exam:&nbsp;<nobr>0.1089 с</nobr>;<br>
+&nbsp;Запросов: 38 (0.0279 с)<br>
+ <br><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
