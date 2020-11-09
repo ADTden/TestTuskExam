ffa8b1cd747172687d4cc3c647e30bbc4cba23d6
ADTden <33018048+ADTden@users.noreply.github.com>
2020-11-09 16:19:47 +0300

    Оценить скорость работы сайта – найти самую долгую страницу и самый долгий компонент
    Задача выполнена

--- ex2/time_control/index.php
+++ ex2/time_control/index.php
@@ -1,12 +1,12 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Оценка производительности");
-?>Больше всего времени потребовалось странице&nbsp;<a href="http://testtusk/bitrix/admin/perfmon_hit_list.php?lang=ru&set_filter=Y&find_script_name=%2Fproducts%2Findex.php">/products/index.php</a>&nbsp;- 3.8 сек.<br>
+?>Больше всего времени потребовалось странице&nbsp;<a href="http://testtusk/bitrix/admin/perfmon_hit_list.php?lang=ru&set_filter=Y&find_script_name=%2Fex2%2Fsimplecomp%2Findex.php">/ex2/simplecomp/index.php</a>&nbsp;- 3.27 сек.<br>
  <br>
- Самая ресурсоемкая страница -&nbsp;<a href="http://testtusk/bitrix/admin/perfmon_hit_list.php?lang=ru&set_filter=Y&find_script_name=%2Fcontacts%2Findex.php">/contacts/index.php</a><br>
- Нагрузка&nbsp;35.23%<br>
-<br>
-При множестве ключей кеша в компоненте, его объем состовляет 119 Кб<br>
-При определенных ключах - 5 Кб<br>
-<br>
-Разница в кеше 114 КБ<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+ Самая ресурсоемкая страница -&nbsp;&nbsp;<a href="http://testtusk/bitrix/admin/perfmon_hit_list.php?lang=ru&set_filter=Y&find_script_name=%2Fex2%2Fsimplecomp%2Findex.php">/ex2/simplecomp/index.php</a><br>
+ Нагрузка&nbsp;14.24%<br>
+ <br>
+ Самый ресурсоемкий компонент<br>
+ Компонент&nbsp;dv:simplecomp.exam:<br>
+ <nobr>0.136 с</nobr>;&nbsp;<br>
+<br><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
