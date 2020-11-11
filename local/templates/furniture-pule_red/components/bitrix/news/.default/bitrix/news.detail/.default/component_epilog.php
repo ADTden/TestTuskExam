e6c25ec3915da32184866ee7fbbbfdf6150bb5bf
ADTden <33018048+ADTden@users.noreply.github.com>
2020-11-11 10:21:05 +0300

    Доработка

--- local/templates/furniture-pule_red/components/bitrix/news/.default/bitrix/news.detail/.default/component_epilog.php
+++ local/templates/furniture-pule_red/components/bitrix/news/.default/bitrix/news.detail/.default/component_epilog.php
@@ -1,11 +1,5 @@
 <?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
-if (isset($arResult['ID_CANONICAL'])){
-	$arSelect = Array("NAME");
-	$arFilter = Array("IBLOCK_ID"=>$arParams["DISPLAY_REL_ID"], "PROPERTY_NEWS"=> $arResult['ID_CANONICAL'], "ACTIVE"=>"Y");
-	$res = CIBlockElement::GetList(Array(), $arFilter, false, Array(false), $arSelect);
-	while($ob = $res->GetNextElement())
-	{
-	 $arFields = $ob->GetFields();
-	 $APPLICATION->SetPageProperty("canonical_link","<link rel='canonical' href=".$arFields["NAME"].">");
-	}
+
+if (isset($arResult['REL_CANONICAL'])){
+	 $APPLICATION->SetPageProperty("canonical_link",$arResult['REL_CANONICAL']);
 }
--- local/templates/furniture-pule_red/components/bitrix/news/.default/bitrix/news.detail/.default/result_modifier.php
+++ local/templates/furniture-pule_red/components/bitrix/news/.default/bitrix/news.detail/.default/result_modifier.php
@@ -1,14 +1,26 @@
 <?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+
+$relLink="";
+$arSelect = Array("NAME");
+	$arFilter = Array("IBLOCK_ID"=>$arParams["DISPLAY_REL_ID"], "PROPERTY_NEWS"=> $arResult["ID"], "ACTIVE"=>"Y");
+	$res = CIBlockElement::GetList(Array(), $arFilter, false, Array(false), $arSelect);
+	while($ob = $res->GetNextElement())
+	{
+	 $arFields = $ob->GetFields();
+	 $relLink="<link rel='canonical' href=".$arFields["NAME"].">";
+	
+	}
+
 $cp = $this->__component; // объект компонента
 
 if (is_object($cp))
 {
    // добавим в arResult компонента поля
-   $cp->arResult['ID_CANONICAL'] = $arResult["ID"];
-   $cp->SetResultCacheKeys(array('ID_CANONICAL'));
+   $cp->arResult['REL_CANONICAL'] = $relLink;
+   $cp->SetResultCacheKeys(array('REL_CANONICAL'));
 
    // сохраним их в копии arResult, с которой работает шаблон
-   $arResult['ID_CANONICAL'] = $cp->arResult['ID_CANONICAL'];
+   $arResult['REL_CANONICAL'] = $cp->arResult['REL_CANONICAL'];
 }
 
 
