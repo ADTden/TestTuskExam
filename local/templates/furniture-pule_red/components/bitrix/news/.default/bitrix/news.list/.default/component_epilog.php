657f238878fa80f6843f7dbae489285e537849b5
ADTden <33018048+ADTden@users.noreply.github.com>
2020-11-11 09:21:09 +0300

    Доработка

--- local/templates/furniture-pule_red/components/bitrix/news/.default/bitrix/news.list/.default/component_epilog.php
+++ local/templates/furniture-pule_red/components/bitrix/news/.default/bitrix/news.list/.default/component_epilog.php
@@ -1,9 +1,10 @@
 <?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
 
 global $APPLICATION;
-echo $cp->arResult['SPECIALDATE'];
-if (isset($arResult['SPECIALDATE']) && $arParams["DISPLAY_SPECIALDATE"] == "Y")
-	echo $cp->arResult['SPECIALDATE'];
-   	$APPLICATION->SetPageProperty("specialdate",$cp->arResult['SPECIALDATE'])
+
+if (isset($arResult['SPECIALDATE']) && $arParams["DISPLAY_SPECIALDATE"] == "Y"){
+
+   	$APPLICATION->SetPageProperty("specialdate",$arResult['SPECIALDATE']);
+}
 ?>
 
